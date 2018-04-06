aff620e1f4897a99737436d4c46371179744737a
kartik <kartikbvarma@icloud.com>
2018-04-05 20:28:41 -0400

    comments and formating

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,15 @@
 from flask import Flask, send_file
 import urllib
 
-app = Flask(__name__) # Creats an app object from flask
+app = Flask(__name__)  # Creats an app object from flask
+
 
 @app.route("/")
 def hello():
-    #return "Howdy!"
+    # return "Howdy!"
     return send_file('images/howdy.jpg', mimetype='image/jpg')
+
 
 if __name__ == "__main__":
     # app.run starts the server.
-    app.run() 
+    app.run()
