0a482607244858bd0bbd31fc9c68c0bf9731f2cc
kartik <kartikbvarma@icloud.com>
2018-04-05 20:26:12 -0400

    sending an howdy image

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, send_file
+import urllib
+
 app = Flask(__name__) # Creats an app object from flask
 
 @app.route("/")
 def hello():
-    return "Howdy!"
+    #return "Howdy!"
+    return send_file('images/howdy.jpg', mimetype='image/jpg')
 
 if __name__ == "__main__":
     # app.run starts the server.
